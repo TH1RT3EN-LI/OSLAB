115c9a83e624fbe8b8478602d795cfc66599df09
buaa g306 <you@example.com>
2025-06-17 16:52:33 +0800

    Initial lab2

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -59,15 +59,15 @@ int readelf(u_char *binary, int size)
         }
 
         // get section table addr, section header number and section header size.
-        ptr_sh_table   = binary + ehdr->e_shoff; // 偏移
-        sh_entry_count = ehdr->e_shnum; // 计数
-        sh_entry_size  = ehdr->e_shentsize; // 大小
+	ptr_sh_table = binary + ehdr->e_shoff;
+	sh_entry_count = ehdr->e_shnum;
+	sh_entry_size = ehdr->e_shentsize;
+
         // for each section header, output section number and section addr.
-        for (Nr = 0; Nr < sh_entry_count; Nr++) {
-            shdr = (Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size);
-            /* 序号用 %d，地址用 0x%x */
-            printf("%d:0x%x\n", Nr, shdr->sh_addr);
-        }
+	for (Nr = 0; Nr < sh_entry_count; Nr++, ptr_sh_table += sh_entry_size) {
+		shdr = (Elf32_Shdr *)ptr_sh_table;
+		printf("%d:0x%x\n", Nr, shdr->sh_addr);
+	}
 
         return 0;
 }
