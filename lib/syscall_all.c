0e36ebc014ebb020dd3568882005a4379728c930
13 <litianshun.cn@gmail.com>
2025-06-27 11:16:17 +0000

    a

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -7,6 +7,7 @@
 
 extern char *KERNEL_SP;
 extern struct Env *curenv;
+extern int syscall_sched_forceReSchedule;
 
 /* Overview:
  * 	This function is used to print a character on screen.
@@ -61,14 +62,13 @@ u_int sys_getenvid(void)
  * Post-Condition:
  * 	Deschedule current environment. This function will never return.
  */
+/*** exercise 4.6 ***/
 void sys_yield(void)
 {
-	/*
-	bcopy((void *)KERNEL_SP - sizeof(struct Trapframe), &(curenv->env_tf), sizeof(struct Trapframe));
-	curenv->env_tf.pc = curenv->env_tf.cp0_epc;
-	curenv = NULL;
-	*/
-	bcopy((void *)KERNEL_SP - sizeof(struct Trapframe), (void *)TIMESTACK - sizeof(struct Trapframe), sizeof(struct Trapframe));
+//     bcopy((void *)KERNEL_SP - sizeof(struct Trapframe),
+//             (void *)TIMESTACK - sizeof(struct Trapframe),
+//             sizeof(struct Trapframe));
+// 	syscall_sched_forceReSchedule = 1;
 	sched_yield();
 }
 
@@ -112,15 +112,15 @@ int sys_env_destroy(int sysno, u_int envid)
  * 	exception stack will be set to `xstacktop`.
  * 	Returns 0 on success, < 0 on error.
  */
+/*** exercise 4.12 ***/
 int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
 {
 	// Your code here.
 	struct Env *env;
 	int ret;
 
-	ret = envid2env(envid, &env, 1);
-	if (ret < 0)
-	{
+    ret = envid2env(envid, &env, 1);
+	if (ret < 0) {
 		return ret;
 	}
 
@@ -128,7 +128,6 @@ int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
 	env->env_xstacktop = xstacktop;
 
 	return 0;
-	//	panic("sys_set_pgfault_handler not implemented");
 }
 
 /* Overview:
@@ -148,38 +147,37 @@ int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
  *	- va must be < UTOP
  *	- env may modify its own address space or the address space of its children
  */
+/*** exercise 4.3 ***/
 int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
 {
 	// Your code here.
 	struct Env *env;
 	struct Page *ppage;
 	int ret;
-	//printf("sys_mem_alloc %x %x\n",envid, va);
-	ret = 0;
-	if (va >= UTOP)
-	{
+    
+    u_int round_va = ROUNDDOWN(va, BY2PG);
+
+	if ((perm & PTE_V) == 0 || (perm & PTE_COW) != 0) {
+        return -E_INVAL;
+	}
+
+	if (round_va >= UTOP) {
 		return -E_INVAL;
 	}
-	if (perm & PTE_COW)
-	{
-		return -E_INVAL;
-	}
+
 	ret = envid2env(envid, &env, 1);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		return ret;
 	}
+
 	ret = page_alloc(&ppage);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		return ret;
 	}
-	ret = page_insert(env->env_pgdir, ppage, va, perm);
-	if (ret < 0)
-	{
-		return ret;
-	}
-	return 0;
+
+	ret = page_insert(env->env_pgdir, ppage, round_va, perm);
+	
+	return ret;
 }
 
 /* Overview:
@@ -195,6 +193,7 @@ int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
  * Note:
  * 	Cannot access pages above UTOP.
  */
+/*** exercise 4.4 ***/
 int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
 				u_int perm)
 {
@@ -203,54 +202,38 @@ int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
 	struct Env *srcenv;
 	struct Env *dstenv;
 	struct Page *ppage;
-	Pte *ppte;
-	//printf("map %x %x ",srcid, dstid);
+	Pte *pte;
+
 	ppage = NULL;
 	ret = 0;
 	round_srcva = ROUNDDOWN(srcva, BY2PG);
 	round_dstva = ROUNDDOWN(dstva, BY2PG);
 
-    //your code her
-
-	if (srcva >= UTOP || dstva >= UTOP)
-	{
-		return -E_INVAL;
-	}
-	// shoule i need to check PTE_V?
-	//if (perm & PTE_COW)
-	//{
-	//	return -E_INVAL;
-	//}
-	ret = envid2env(srcid, &srcenv, 1);
-	if (ret < 0)
-	{
-		return ret;
-	}
-	ret = envid2env(dstid, &dstenv, 1);
-	if (ret < 0)
-	{
-		return ret;
-	}
-	
-	//printf("src:%x %x dst:%x %x %x\n", srcenv->env_pgdir, srcva,dstenv->env_pgdir, dstva, perm);
-	ppage = page_lookup(srcenv->env_pgdir, round_srcva, &ppte);
-	if (ppage == NULL)
-	{
-		return -E_INVAL;
-	}
-	if (((*ppte & PTE_R) == 0) && ((perm & PTE_R) == 1))
-	{
+    //your code here
+    if ((perm & PTE_V) == 0) {
 		return -E_INVAL;
 	}
 
-	ppage = pa2page(PTE_ADDR(*ppte));
+	if (round_srcva >= UTOP || round_dstva >= UTOP) {
+		return -E_INVAL;
+	}
+
+	ret = envid2env(srcid, &srcenv, 0);
+	if (ret < 0) {
+		return ret;
+	}
+	ret = envid2env(dstid, &dstenv, 0);
+	if (ret < 0) {
+		return ret;
+	}
+
+	ppage = page_lookup(srcenv->env_pgdir, round_srcva, &pte);
+	if (ppage == NULL || ((*pte) & PTE_V) == NULL) {
+		return -E_INVAL;
+	}
+
 	ret = page_insert(dstenv->env_pgdir, ppage, round_dstva, perm);
-	if (ret < 0)
-	{
-		return ret;
-	}
 
-	ret = 0;
 	return ret;
 }
 
@@ -263,28 +246,27 @@ int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
  *
  * Cannot unmap pages above UTOP.
  */
+/*** exercise 4.5 ***/
 int sys_mem_unmap(int sysno, u_int envid, u_int va)
 {
-	//printf("unmap %x %x\n",envid, va);
 	// Your code here.
-	int ret;
+    int ret = 0;
 	struct Env *env;
 
-	if (va >= UTOP)
-	{
+	u_int round_va = ROUNDDOWN(va, BY2PG);
+
+	if (round_va >= UTOP) {
 		return -E_INVAL;
 	}
 
-	ret = envid2env(envid, &env, 1);
-	if(ret < 0)
-	{
+	ret = envid2env(envid, &env, 0);
+	if (ret < 0) {
 		return ret;
 	}
 
-	page_remove(env->env_pgdir, va);
+	page_remove(env->env_pgdir, round_va);
 
 	return ret;
-	//	panic("sys_mem_unmap not implemented");
 }
 
 /* Overview:
@@ -299,24 +281,27 @@ int sys_mem_unmap(int sysno, u_int envid, u_int va)
  * 	In the child, the register set is tweaked so sys_env_alloc returns 0.
  * 	Returns envid of new environment, or < 0 on error.
  */
+/*** exercise 4.8 ***/
 int sys_env_alloc(void)
 {
 	// Your code here.
 	int r;
 	struct Env *e;
-	r = env_alloc(&e, curenv->env_id);
-	if (r < 0)
-	{
+
+    r = env_alloc(&e, curenv->env_id);
+	if (r < 0) {
 		return r;
 	}
-	e->env_status = ENV_NOT_RUNNABLE;
-	e->env_pri = curenv->env_pri;
-	bcopy((void *)KERNEL_SP - sizeof(struct Trapframe), (void *)&(e->env_tf), sizeof(struct Trapframe));
-	e->env_tf.pc = e->env_tf.cp0_epc;
-	e->env_tf.regs[2] = 0;
+	bcopy((void *)KERNEL_SP - sizeof(struct Trapframe),
+            (void *)(&(e->env_tf)),
+            sizeof(struct Trapframe));
 
-	return e->env_id;
-	//	panic("sys_env_alloc not implemented");
+	e->env_status = ENV_NOT_RUNNABLE;
+	e->env_tf.regs[2] = 0;
+	e->env_pri = curenv->env_pri;
+    e->env_tf.pc = e->env_tf.cp0_epc;
+    
+    return e->env_id;
 }
 
 /* Overview:
@@ -331,45 +316,31 @@ int sys_env_alloc(void)
  * 	Return -E_INVAL if status is not a valid status for an environment.
  * 	The status of environment will be set to `status` on success.
  */
+/*** exercise 4.14 ***/
 int sys_set_env_status(int sysno, u_int envid, u_int status)
 {
 	// Your code here.
 	struct Env *env;
 	int ret;
-	extern struct Env_list env_sched_list[];
-	struct Env *o;
-	if (status != ENV_RUNNABLE && status != ENV_NOT_RUNNABLE && status != ENV_FREE)
-	{
+
+    if (status != ENV_RUNNABLE && status != ENV_NOT_RUNNABLE && status != ENV_FREE) {
 		return -E_INVAL;
 	}
+
 	ret = envid2env(envid, &env, 1);
-	if (ret < 0)
-	{
-		return ret;
+	if (ret < 0) {
+		return -E_INVAL;
 	}
-	//printf("set status %x %d %x\n",envid, env->env_status, status);
-	/*
-	LIST_FOREACH(o, &env_sched_list[0], env_sched_link)
-	{
-		printf("%x ",o->env_id);
-	}
-	printf("\n");
-	LIST_FOREACH(o, &env_sched_list[1], env_sched_link)
-	{
-		printf("%x ",o->env_id);
-	}
-	printf("\n");
-	*/
-	if (status == ENV_FREE)
-	{
+
+	env->env_status = status;
+	
+    if (status == ENV_RUNNABLE) {
+        LIST_INSERT_HEAD(&env_sched_list[0], env, env_sched_link);
+    } else if (status == ENV_FREE) {
 		env_destroy(env);
 	}
-	else
-	{
-		env->env_status = status;
-	}
+
 	return 0;
-	//	panic("sys_env_set_status not implemented");
 }
 
 /* Overview:
@@ -418,18 +389,18 @@ void sys_panic(int sysno, char *msg)
  * 	This syscall will set the current process's status to 
  * ENV_NOT_RUNNABLE, giving up cpu. 
  */
+/*** exercise 4.7 ***/
 void sys_ipc_recv(int sysno, u_int dstva)
 {
-	if (dstva >= UTOP)
-	{
+    int isValid = (dstva < UTOP) || (dstva == NULL);
+	if (!isValid) {
 		return;
 	}
-
 	curenv->env_ipc_recving = 1;
 	curenv->env_ipc_dstva = dstva;
 	curenv->env_status = ENV_NOT_RUNNABLE;
-	sys_yield();
-	//cannot back
+
+    sys_yield();
 }
 
 /* Overview:
@@ -449,45 +420,117 @@ void sys_ipc_recv(int sysno, u_int dstva)
  *
  * Hint: the only function you need to call is envid2env.
  */
+/*** exercise 4.7 ***/
 int sys_ipc_can_send(int sysno, u_int envid, u_int value, u_int srcva,
 					 u_int perm)
 {
-
-	int r;
+    int r;
 	struct Env *e;
 	struct Page *p;
-	Pte *pte;
-
-	if (srcva >= UTOP)
-	{
-		return -E_INVAL;
-	}
 
 	r = envid2env(envid, &e, 0);
-	if (r < 0)
-	{
+	if (r < 0) {
 		return r;
 	}
 
-	if (e->env_ipc_recving == 0)
-	{
+	if (!e->env_ipc_recving) {
 		return -E_IPC_NOT_RECV;
+	}
+
+	if (srcva != NULL) {
+		Pte *pte;
+		u_int round_srcva = ROUNDDOWN(srcva, BY2PG);
+		p = page_lookup(curenv->env_pgdir, round_srcva, &pte);
+		if (p == NULL || ((*pte) & PTE_V) == NULL) {
+			return -E_INVAL;
+		}
+		u_int round_dstva = ROUNDDOWN(e->env_ipc_dstva, BY2PG);
+		r = page_insert(e->env_pgdir, p, round_dstva, perm);
+		if (r < 0) {
+			return r;
+		}
+		e->env_ipc_perm = perm;
 	}
 
 	e->env_ipc_value = value;
 	e->env_ipc_from = curenv->env_id;
-	e->env_ipc_perm = perm;
 	e->env_ipc_recving = 0;
 	e->env_status = ENV_RUNNABLE;
-	
-	if (srcva != 0)
-	{
-		p = page_lookup(curenv->env_pgdir, srcva, &pte);
-		if (p == NULL)
-		{
-			return -E_INVAL;
-		}
-		page_insert(e->env_pgdir, p, e->env_ipc_dstva, perm);
+
+	return 0;
+}
+
+int isValidDev(u_int addr, u_int len) {
+	return (addr >= 0x10000000 && addr + len <= 0x10000020) ||
+		   (addr >= 0x13000000 && addr + len <= 0x13004200) ||
+		   (addr >= 0x15000000 && addr + len <= 0x15000200);
+}
+
+/* Overview:
+ * 	This function is used to write data to device, which is
+ * 	represented by its mapped physical address.
+ *	Remember to check the validity of device address (see Hint below);
+ * 
+ * Pre-Condition:
+ *      'va' is the startting address of source data, 'len' is the
+ *      length of data (in bytes), 'dev' is the physical address of
+ *      the device
+ * 	
+ * Post-Condition:
+ *      copy data from 'va' to 'dev' with length 'len'
+ *      Return 0 on success.
+ *	Return -E_INVAL on address error.
+ *      
+ * Hint: Use ummapped segment in kernel address space to perform MMIO.
+ *	 Physical device address:
+ *	* ---------------------------------*
+ *	|   device   | start addr | length |
+ *	* -----------+------------+--------*
+ *	|  console   | 0x10000000 | 0x20   |
+ *	|    IDE     | 0x13000000 | 0x4200 |
+ *	|    rtc     | 0x15000000 | 0x200  |
+ *	* ---------------------------------*
+ */
+int sys_write_dev(int sysno, u_int va, u_int dev, u_int len)
+{
+    if (!isValidDev(dev, len)) {
+		return -E_INVAL;
 	}
+
+	volatile u_char *src = (u_char *)(va);
+	volatile u_char *kva = (u_char *)(dev + 0xA0000000);
+
+    bcopy(src, kva, len);
+
+	return 0;
+}
+
+/* Overview:
+ * 	This function is used to read data from device, which is
+ * 	represented by its mapped physical address.
+ *	Remember to check the validity of device address (same as sys_read_dev)
+ * 
+ * Pre-Condition:
+ *      'va' is the startting address of data buffer, 'len' is the
+ *      length of data (in bytes), 'dev' is the physical address of
+ *      the device
+ * 
+ * Post-Condition:
+ *      copy data from 'dev' to 'va' with length 'len'
+ *      Return 0 on success, < 0 on error
+ *      
+ * Hint: Use ummapped segment in kernel address space to perform MMIO.
+ */
+int sys_read_dev(int sysno, u_int va, u_int dev, u_int len)
+{
+    if (!isValidDev(dev, len)) {
+		return -E_INVAL;
+	}
+
+	volatile u_char *dst = (u_char *)(va);
+	volatile u_char *kva = (u_char *)(dev + 0xA0000000);
+
+    bcopy(kva, dst, len);
+
 	return 0;
 }
--- user/.ipynb_checkpoints/file-checkpoint.c
+++ user/.ipynb_checkpoints/file-checkpoint.c
@@ -8,9 +8,6 @@ static int file_read(struct Fd *fd, void *buf, u_int n, u_int offset);
 static int file_write(struct Fd *fd, const void *buf, u_int n, u_int offset);
 static int file_stat(struct Fd *fd, struct Stat *stat);
 
-
-// Dot represents choosing the variable of the same name within struct declaration 
-// to assign, and no need to consider order of variables.
 struct Dev devfile = {
 	.dev_id =	'f',
 	.dev_name =	"file",
@@ -30,7 +27,7 @@ struct Dev devfile = {
 int
 open(const char *path, int mode)
 {
-    struct Fd *fd;
+	struct Fd *fd;
 	struct Filefd *ffd;
 	u_int size, fileid;
 	int r;
@@ -39,39 +36,22 @@ open(const char *path, int mode)
 
 	// Step 1: Alloc a new Fd, return error code when fail to alloc.
 	// Hint: Please use fd_alloc.
-	r = fd_alloc(&fd);
-	if (r) {
-		return r;
-	}
+
 
 	// Step 2: Get the file descriptor of the file to open.
-	// Hint: Read fsipc.c, and choose a function.
-	r = fsipc_open(path, mode, fd);
-	if (r) {
-		return r;
-	}
+
 
 	// Step 3: Set the start address storing the file's content. Set size and fileid correctly.
 	// Hint: Use fd2data to get the start address.
-	ffd = (struct Filefd *)fd;
-	fileid = ffd->f_fileid;
-	size = ffd->f_file.f_size;
 
-	va = fd2data(fd);
 
-	// Step 4: Alloc memory, map the file content into memory.
-	if (size == 0) {
-		return fd2num(fd);
-	}
-	for (i = 0; i < size; i += BY2PG) {
-		if ((r = fsipc_map(fileid, i, va + i)) < 0) {
-			writef("Can not map the file.\n");
-			return r;
-		}
-	}
+	// Step 4: Map the file content into memory.
 
-	// Step 5: Return the number of file descriptor.
-	return fd2num(fd);
+
+	// Step 5: Return file descriptor.
+	// Hint: Use fd2num.
+
+	
 }
 
 // Overview:
@@ -267,13 +247,11 @@ ftruncate(int fdnum, u_int size)
 }
 
 // Overview:
-//	Delete a file or directory.
+//	Delete a file/directory.
 int
 remove(const char *path)
 {
 	// Your code here.
-	// Call fsipc_remove.
-	return fsipc_remove(path);
 }
 
 // Overview:
@@ -283,3 +261,4 @@ sync(void)
 {
 	return fsipc_sync();
 }
+
