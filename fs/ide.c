6d240e3b5c6aff3d5ea4be4664bbbd75885e81c7
TH1RT3EN-LI <litianshun.cn@gmail.com>
2025-06-29 19:01:07 +0800

    half lab5

--- fs/ide.c
+++ fs/ide.c
@@ -21,103 +21,49 @@
 // 	If error occurred during read the IDE disk, panic. 
 // 	
 // Hint: use syscalls to access device registers and buffers
-void
-ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
-{
-	// 0x200: the size of a sector: 512 bytes.
-	u_int offset_begin = secno * 0x200;
-	u_int offset_end = offset_begin + nsecs * 0x200;
+static void ide_transfer(u_int diskno, u_int secno, void *buf, u_int nsecs, int is_write) {
+	const u_int SECTOR_SIZE = 0x200;
+	const u_int DEV_ADDR = 0x13000000;
+	u_int offset_begin = secno * SECTOR_SIZE;
+	u_int offset_end = offset_begin + nsecs * SECTOR_SIZE;
 	u_int offset = 0;
-	u_int dev_addr = 0x13000000;
 	u_char status = 0;
-	u_char read_value = 0;
+	u_char rw_value = is_write ? 1 : 0;
 
 	while (offset_begin + offset < offset_end) {
 		u_int now_offset = offset_begin + offset;
-		if (syscall_write_dev((u_int)&diskno, dev_addr + 0x10, 4) < 0)
-		{
-			user_panic("ide_read error!");
+		if (syscall_write_dev((u_int)&diskno, DEV_ADDR + 0x10, 4) < 0)
+			user_panic("ide_transfer: diskno write error!");
+		if (syscall_write_dev((u_int)&now_offset, DEV_ADDR + 0x0, 4) < 0)
+			user_panic("ide_transfer: offset write error!");
+
+		if (is_write) {
+			if (syscall_write_dev((u_int)(buf + offset), DEV_ADDR + 0x4000, SECTOR_SIZE) < 0)
+				user_panic("ide_transfer: data write error!");
 		}
-		if (syscall_write_dev((u_int)&now_offset, dev_addr + 0x0, 4) < 0)
-		{
-			user_panic("ide_read error!");
-		}
-		if (syscall_write_dev((u_int)&read_value, dev_addr + 0x20, 1) < 0)
-		{
-			user_panic("ide_read error!");
-		}
+
+		if (syscall_write_dev((u_int)&rw_value, DEV_ADDR + 0x20, 1) < 0)
+			user_panic("ide_transfer: rw_value write error!");
+
 		status = 0;
-		if (syscall_read_dev((u_int)&status, dev_addr + 0x30, 1) < 0)
-		{
-			user_panic("ide_read error!");
-		}
+		if (syscall_read_dev((u_int)&status, DEV_ADDR + 0x30, 1) < 0)
+			user_panic("ide_transfer: status read error!");
 		if (status == 0)
-		{
-			user_panic("ide read faild!");
+			user_panic(is_write ? "ide write failed!" : "ide read failed!");
+
+		if (!is_write) {
+			if (syscall_read_dev((u_int)(buf + offset), DEV_ADDR + 0x4000, SECTOR_SIZE) < 0)
+				user_panic("ide_transfer: data read error!");
 		}
-		if (syscall_read_dev((u_int)(dst + offset), dev_addr + 0x4000, 0x200) < 0)
-		{
-			user_panic("ide_read error!");
-		}
-		offset += 0x200;
+
+		offset += SECTOR_SIZE;
 	}
-	//writef("ide_read %x %s\n", offset_begin, dst);
 }
 
+void ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs) {
+	ide_transfer(diskno, secno, dst, nsecs, 0);
+}
 
-// Overview:
-// 	write data to IDE disk.
-//
-// Parameters:
-//	diskno: disk number.
-//	secno: start sector number.
-// 	src: the source data to write into IDE disk.
-//	nsecs: the number of sectors to write.
-//
-// Post-Condition:
-//	If error occurred during read the IDE disk, panic.
-//	
-// Hint: use syscalls to access device registers and buffers
-void
-ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
-{
-	u_int offset_begin = secno * 0x200;
-	u_int offset_end = offset_begin + nsecs * 0x200;
-	u_int offset = 0;
-	u_int dev_addr = 0x13000000;
-	u_char status = 0;
-	u_char write_value = 1;
-
-	writef("diskno: %d\n", diskno);
-
-	while (offset_begin + offset < offset_end) {
-		u_int now_offset = offset_begin + offset;
-		if (syscall_write_dev((u_int)&diskno, dev_addr + 0x10, 4) < 0)
-		{
-			user_panic("ide_write error!");
-		}
-		if (syscall_write_dev((u_int)&now_offset, dev_addr + 0x0, 4) < 0)
-		{
-			user_panic("ide_write error!");
-		}
-		if (syscall_write_dev((u_int)(src + offset), dev_addr + 0x4000, 0x200) < 0)
-		{
-			user_panic("ide_write error!");
-		}
-		if (syscall_write_dev((u_int)&write_value, dev_addr + 0x20, 1) < 0)
-		{
-			user_panic("ide_write error!");
-		}
-		status = 0;
-		if (syscall_read_dev((u_int)&status, dev_addr + 0x30, 1) < 0)
-		{
-			user_panic("ide_write error!");
-		}
-		if (status == 0)
-		{
-			user_panic("ide write faild!");
-		}
-		offset += 0x200;
-	}
-	//writef("ide_write %x %s\n", offset_begin, src);
+void ide_write(u_int diskno, u_int secno, void *src, u_int nsecs) {
+	ide_transfer(diskno, secno, src, nsecs, 1);
 }
